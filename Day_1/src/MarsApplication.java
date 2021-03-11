dcdbf2f252b7f8eddb917024d6b1a7a8996e23a7
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2021-03-11 16:40:16 +0100

    Add files via upload

---
+++ Day_1/src/MarsApplication.java
@@ -0,0 +1,27 @@
+class MarsApplication {
+    public static void main(String[] arguments) {
+        MarsRobot spirit = new MarsRobot();
+        spirit.status = "eksploracja";
+        spirit.speed = 2;
+        spirit.temperature = -60;
+
+        spirit.showAttributes();
+        System.out.println("Zwiększenie prędkości do 3.");
+        spirit.speed = 3;
+        spirit.showAttributes();
+        System.out.println("Zmiana temperatury na -90.");
+        spirit.temperature = -90;
+        spirit.showAttributes();
+        System.out.println("Sprawdzenie temperatury.");
+        spirit.checkTemperature();
+        spirit.showAttributes();
+
+        MarsRobot opportunity = new MarsRobot();
+        System.out.println("");
+        opportunity.speed = 5;
+        opportunity.status = "powrót do domu";
+        opportunity.temperature = -88;
+        opportunity.showAttributes();
+
+    }
+}
