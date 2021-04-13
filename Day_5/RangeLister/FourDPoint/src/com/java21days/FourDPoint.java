74f4c125d2d6b95bc4605acabc3ecd52014e3150
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2021-04-13 14:44:36 +0200

    Add files via upload

---
+++ Day_5/RangeLister/FourDPoint/src/com/java21days/FourDPoint.java
@@ -0,0 +1,22 @@
+package com.java21days;
+
+import java.awt.*;
+
+public class FourDPoint extends Point {
+    int z;
+    int t;
+
+    FourDPoint(int x, int y, int inZ, int inT) {
+        super(x,y);
+        this.z = inZ;
+        this.t = inT;
+    }
+
+    public static void main(String[] args) {
+        FourDPoint np = new FourDPoint(5, 5, 4, 100);
+        System.out.println("x wynosi " + np.x);
+        System.out.println("y wynosi " + np.y);
+        System.out.println("z wynosi " + np.z);
+        System.out.println("t wynosi " + np.t);
+    }
+}
