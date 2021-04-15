15bffe3eb2b8fc673ddc8d8281c1302092ed2889
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2021-04-15 13:06:54 +0200

    Add files via upload

---
+++ Day_6/InstanceCounter/Storefront2/src/org/cadenhead/ecommerce/GiftShop2.java
@@ -0,0 +1,21 @@
+package org.cadenhead.ecommerce;
+
+public class GiftShop2 {
+    public static void main(String[] arguments) {
+        Storefront2 store = new Storefront2();
+        store.addItem("C01", "KUBEK", "9.99", "150", "NIE");
+        store.addItem("C02", "DUŻY KUBEK", "12.99", "82", "TAK");
+        store.addItem("C03", "PODKŁADKA", "10.49", "800", "NIE");
+        store.addItem("D01", "KOSZULKA", "16.99", "90", "TAK");
+        store.sort();
+
+        for (int i = 0; i < store.getSize(); i++) {
+            Item2 show = (Item2)store.getItem(i);
+            System.out.println("\nIdentyfikator elementu: " + show.getId() +
+                    "\nNazwa: " + show.getName() +
+                    "\nCena sprzedaży: " + show.getRetail() + " zł" +
+                    "\nCena: " + show.getPrice() + " zł" +
+                    "\nSztuk: " + show.getQuantity());
+        }
+    }
+}
